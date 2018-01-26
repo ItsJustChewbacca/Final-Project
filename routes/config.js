bfd283d3268a634f2a0a5f81e7dcecf715601d99
ItsJustChewbacca <itsjustchewbacca@gmail.com>
2018-01-26 22:39:22 +0000

    no changes

--- routes/config.js
+++ routes/config.js
@@ -4,7 +4,8 @@ const express = require('express');
 const router  = express.Router();
 
 router.get('/', (req, res) => {
-  res.send("Configuration Page")
+
+  res.render("config");
 });
 
 module.exports = router;
