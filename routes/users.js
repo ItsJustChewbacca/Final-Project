2e236e9d3b78bc761d2cd34ab00595883e4d804d
Payal Dhiman <payaldhiman@outlook.com>
2018-01-27 01:30:18 +0000

    removed users object

--- routes/users.js
+++ routes/users.js
@@ -3,17 +3,7 @@
 const express = require('express');
 const router  = express.Router();
 
-const users = {
-  "user1": {
-    id: "user1"
-    first_name: 'Brandon',
-    last_name: 'Samuel',
-    username: 'bsamuel',
-    email: 'brandonsamuel72@gmail.com',
-    password: '123',
-    confirm_password: '123'
-  }
-};
+
 
 router.get('/login', (req, res) => {
   res.render("login");
