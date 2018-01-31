44932ffd4c11d16165e4e359491f2647859263e9
Payal Dhiman <payaldhiman@outlook.com>
2018-01-31 22:18:15 +0000

    added like/dislike functionality

---
+++ public/scripts/likes.js
@@ -0,0 +1,61 @@
+$(function() {
+
+  $( "body" ).on('click', '.fa-thumbs-o-up', function() {
+
+    if (!$(this).hasClass('up')) {
+      $(this).addClass('up');
+      $('.fa-thumbs-o-down').removeClass('down');
+
+      var likeCounter = $(this).siblings('.likecounter');
+      var count = +likeCounter.text();
+      console.log('count', count);
+      var closestCard = $(this).closest('.like');
+      console.log('closest card:', closestCard);
+      var topicid = $(closestCard).data('id');
+      console.log(topicid);
+
+      count++;
+      likeCounter.text(count);
+    }
+
+
+
+    $.ajax({
+      url: `/forum/topics/${topicid}/likes`,
+      method: 'POST',
+      data: {"likes": count},
+      success: (s) => {
+        console.log("data added ");
+      }
+    });
+
+  });
+
+  $( "body" ).on('click', '.fa-thumbs-o-down', function() {
+    if (!$(this).hasClass('down')) {
+      $(this).addClass('down');
+      $('.fa-thumbs-o-up').removeClass('up');
+
+      var likeCounter = $(this).siblings('.likecounter');
+      var count = +likeCounter.text();
+      console.log('count', count);
+      var closestCard = $(this).closest('.like');
+      console.log('closest card:', closestCard);
+      var topicid = $(closestCard).data('id');
+      console.log(topicid);
+
+      count--;
+      likeCounter.text(count);
+    }
+
+    $.ajax({
+      url: `/forum/topics/${topicid}/likes`,
+      method: 'POST',
+      data: {"likes": count},
+      success: (s) => {
+        console.log("data added ");
+      }
+    });
+
+  });
+});
