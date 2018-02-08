df2effb1a498ffbd28aac166c9c5bcf68cbec803
ItsJustChewbacca <itsjustchewbacca@gmail.com>
2018-02-08 02:14:27 +0000

    updated seeds for demo

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -3,17 +3,18 @@ exports.seed = function(knex, Promise) {
     return knex('topics').del().then(function () {
       return knex('users').del().then(function () {
         return Promise.all([
-          knex('users').insert({id: 1, first_name: 'Alice', last_name: 'hippo', username: 'ilikehippos', email: 'alice@alice.com', password: '123456', confirm_password: '123456' }),
-          knex('users').insert({id: 2, first_name: 'bobbie', last_name: 'elk', username: 'ilikeelk', email: 'bobbie@bobbie.com', password: '123456', confirm_password: '123456'}),
-          knex('users').insert({id: 3, first_name: 'Charlie'})
+          knex('users').insert({id: 1, first_name: 'Alice', last_name: 'bob', username: 'Alice', email: 'alice@alice.com', password: '123456', confirm_password: '123456' }),
+          knex('users').insert({id: 2, first_name: 'bobbie', last_name: 'ace', username: 'Bobbie', email: 'bobbie@bobbie.com', password: '123456', confirm_password: '123456'}),
+          knex('users').insert({id: 3, first_name: 'Charlie', last_name: 'char', username: 'charlie', email: 'charlie@charlie.com', password: '123456', confirm_password: '123456'})
         ]).then(function() {
           return Promise.all([
-            knex('topics').insert({id:1, title: 'this app is awesome', description: 'this app is really cool because videos are awesome', users_id:1}),
-            knex('topics').insert({id:2, title: 'this app sucks', description: 'this app is the worst because i said so', users_id:2}),
+            knex('topics').insert({id:1, title: 'Would be cool if Map-X supported Fortnite', description: 'Would be cool if Map-X supported Fortnite. I really need to have some of the control layouts from Call of Duty WW2', users_id:1}),
+            knex('topics').insert({id:2, title: 'I really like this app!', description: 'Map-X worked aweseome with my new xbox one elite controller, Its great that I dont have to switch between controls constantly', users_id:2}),
+            knex('topics').insert({id:3, title: 'Crazy cool App', description: 'this app is a blast' , users_id:3})
           ]).then(function() {
             return Promise.all([
-              knex('comments').insert({id:1, description: 'your post sucks', users_id: 2, topics_id: 1}),
-              knex('comments').insert({id:2, description: 'your post sucks', users_id: 1, topics_id: 2})
+              knex('comments').insert({id:1, description: 'upvote!', users_id: 2, topics_id: 1}),
+              knex('comments').insert({id:2, description: 'you have my upvote', users_id: 1, topics_id: 2})
             ]);
           });
         });
